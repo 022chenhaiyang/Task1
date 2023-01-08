ed29ec6f1906e80498ad967c74608278a88ce6c9
Chen Haiyang <121848934+022chenhaiyang@users.noreply.github.com>
2023-01-08 14:22:50 +0800

    Create a prototype of the system

---
+++ file_system/file_system_exceptions.py
@@ -0,0 +1,15 @@
+
+class IllegalFileSystemOperation(Exception):
+    pass
+
+
+class PathNotFound(Exception):
+    pass
+
+
+class PathAlreadyExists(Exception):
+    pass
+
+
+class NotATextFile(Exception):
+    pass
